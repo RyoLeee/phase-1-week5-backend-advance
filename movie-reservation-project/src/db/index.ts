5097227df879395bba231dacbaa1b908780f4468
Ryo Ardiansyah <h4285007@gmail.com>
2026-05-01 08:49:46 +0700

    Delete movie-reservation-project directory

--- movie-reservation-project/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/psotgres-js";
-import postgres  from "postgres";
-import * as schema from "./schema";
-import "dotenv/config";
-
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
-
